bab18559301e17873ef85f24f3b95c19f58a480e
gaojie11 <gaojie11@xiaomi.com>
2021-03-19 19:34:15 +0800

    aop

---
+++ src/main/java/com/jlu/masli/gj11/aop/config/LogFilter.java
@@ -0,0 +1,13 @@
+package com.jlu.masli.gj11.aop.config;
+
+import java.lang.annotation.*;
+
+/**
+ * 系统日志元注解
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface LogFilter {
+    String value() default "" ;
+}
---
+++ src/main/java/com/jlu/masli/gj11/controller/AopController.java
@@ -0,0 +1,25 @@
+package com.jlu.masli.gj11.controller;
+
+import com.jlu.masli.gj11.aop.config.LogFilter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AopController {
+
+    @LogFilter("保存请求日志")
+    @RequestMapping("/saveRequestLog")
+    public String saveRequestLog (@RequestParam("name") String name){
+        return "success："+name ;
+    }
+
+    @LogFilter("保存异常日志")
+    @RequestMapping("/saveExceptionLog")
+    public String saveExceptionLog (@RequestParam("name") String name){
+        int error = 100 / 0 ;
+        System.out.println(error);
+        return "success："+name ;
+    }
+}
+
